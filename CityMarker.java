fc523e3bcc6dc498c22cb734a5c93d4285a9c32b
SmartSood <133507159+SmartSood@users.noreply.github.com>
2023-06-16 18:41:53 +0530

    Update CityMarker.java

--- CityMarker.java
+++ CityMarker.java
@@ -8,7 +8,7 @@ import processing.core.PGraphics;
 
 /** Implements a visual marker for cities on an earthquake map
  * 
- * @author UC San Diego Intermediate Software Development MOOC team
+ * @author Smarth Sood and UC San Diego Intermediate Software Development MOOC team
  * 
  */
 public class CityMarker extends CommonMarker {

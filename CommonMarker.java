2ef72d27c96f372f7b81839efd544842105775f7
SmartSood <133507159+SmartSood@users.noreply.github.com>
2023-06-16 18:42:15 +0530

    Update CommonMarker.java

--- CommonMarker.java
+++ CommonMarker.java
@@ -7,7 +7,7 @@ import processing.core.PGraphics;
 
 /** Implements a common marker for cities and earthquakes on an earthquake map
  * 
- * @author UC San Diego Intermediate Software Development MOOC team
+ * @author Smarth Sood and UC San Diego Intermediate Software Development MOOC team
  *
  */
 public abstract class CommonMarker extends SimplePointMarker {
